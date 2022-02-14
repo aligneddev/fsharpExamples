bc9a74f3f2420733bfb93ed72787bb2b1861de9a
Kevin Logan <kevin.logan@ravenind.com>
2022-02-14 15:09:13 -0600

    first commit

---
+++ examples/01SimilarToLinqWithPipes.fs
@@ -0,0 +1,15 @@
+// send to F# Interactive with highlight + Alt + Enter
+// or Open the Command Palette with Ctrl+Shift+P and find FSI: Start command (requires Ionide extension)
+// https://stackoverflow.com/questions/56425674/how-to-enable-f-interactive-in-visual-studio-code
+module fSharpExamples
+
+let getOddSquares xs =
+    xs
+    |> List.filter (fun x -> x % 2 <> 0)
+    |> List.map (fun x -> x * x)
+
+// compare to C#
+// var xs = new List<int>{ 1, 2, 3, 4, 5, 6};
+// var results = xs.Where(x => x % 2 != 0).Select(x => x * x);
+
+printfn "%A" (getOddSquares [1..10])
