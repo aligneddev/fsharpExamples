5f7d4c3364b81f3f9cf7e7967b57961c03d8c64e
Kevin Logan <kevin.logan@ravenind.com>
2022-02-15 08:46:15 -0600

    pattern matching

--- examples/07PatternMatching.fs
+++ examples/07PatternMatching.fs
@@ -1,4 +1,25 @@
-// https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/pattern-matching
+//examples from  https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/pattern-matching
 module fsharpExamples.PatternMatching
 
-// todo
+// match expression with
+// | pattern [ when condition ] -> result-expression
+// ...
+
+type Color =
+    | Red = 0
+    | Green = 1
+    | Blue = 2
+
+let printColorName (color:Color) =
+    match color with
+    | Color.Red -> printfn "Red"
+    | Color.Green -> printfn "Green"
+    | Color.Blue -> printfn "Blue"
+    | _ -> ()
+
+printColorName Color.Red
+printColorName Color.Green
+printColorName Color.Blue
+
+
+// many more
